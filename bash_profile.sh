463a36703525718cf1a5836689471db5a8b10fc1
Derrick Tennant <derrick.tennant@automattic.com>
2019-10-08 16:09:04 +0000

    Added top bar

--- bash_profile.sh
+++ bash_profile.sh
@@ -5,4 +5,8 @@ git config --global user.email "derrick.tennant@automattic.com"
 git config --global user.name "Derrick Tennant"
 git -C /home/vipdev/dev-scripts/go-sandbox/ pull
 
-export PS1="\[\033[38;5;124m\]▐\[$(tput sgr0)\]\[\033[38;5;15m\]\[\033[48;5;124m\][SANDBOXED: \h]\[$(tput sgr0)\]\[\033[38;5;124m\]\[\033[48;5;-1m\]▌\[$(tput sgr0)\]\[\033[38;5;15m\]\n\[$(tput sgr0)\]\[\033[38;5;6m\]\u\[$(tput sgr0)\]\[\033[38;5;3m\]:\[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
+
+tput csr 1 "$((LINES-1))"
+export PS1="\
+$(tput sc)$(tput home)\[\033[38;5;124m\]▐\[$(tput sgr0)\]\[\033[38;5;15m\]\[\033[48;5;124m\][SANDBOXED: \h]\[$(tput sgr0)\]\[\033[38;5;124m\]\[\033[48;5;-1m\]▌\[$(tput sgr0)\]\[\033[38;5;15m\]\
+$(tput rc)\[$(tput sgr0)\]\[\033[38;5;6m\]\u\[$(tput sgr0)\]\[\033[38;5;3m\]:\[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
