3468223b9b098abf7659fd7c2a42cacc4a33d279
Derrick Tennant <derrick.tennant@automattic.com>
2019-10-08 15:59:03 +0000

    Added custom prompt, renamed file

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 alias logs="tail -F /tmp/php-errors -F /chroot/tmp/php-errors"
 alias run-crons="wp site list --path=/chroot/var/www --field=url | xargs -I URL wp --path=/chroot/var/www cron event run --due-now --url=URL"
 git config --global user.email "derrick.tennant@automattic.com"
 git config --global user.name "Derrick Tennant"
 git -C /home/vipdev/dev-scripts/go-sandbox/ pull
 
+export PS1="\[\033[38;5;124m\]▐\[$(tput sgr0)\]\[\033[38;5;15m\]\[\033[48;5;124m\][SANDBOXED: \h]\[$(tput sgr0)\]\[\033[38;5;124m\]\[\033[48;5;-1m\]▌\[$(tput sgr0)\]\[\033[38;5;15m\]\n\[$(tput sgr0)\]\[\033[38;5;6m\]\u\[$(tput sgr0)\]\[\033[38;5;3m\]:\[$(tput sgr0)\]\[\033[38;5;2m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]\\$ \[$(tput sgr0)\]"
