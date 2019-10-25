0d648696e080433ac32e7c7be6aea173aa718324
Derrick Tennant <emrikol@users.noreply.github.com>
2019-10-25 15:40:15 +0200

    vip_dump updates :)

--- mu-plugins/00-sandbox-helper.php
+++ mu-plugins/00-sandbox-helper.php
@@ -3,16 +3,17 @@ ini_set( 'xdebug.cli_color', 1 );
 
 if ( ! function_exists( 'vip_dump' ) ) {
 	function vip_dump( $var = null ) {
-		$prefix = '';
-		if ( $microtime ) {
-			$prefix = sprintf( '[%s] ', microtime() );
-		}
+		$old_setting = ini_get( 'html_errors' );
+		ini_set( 'html_errors', false );
+		ini_set( 'xdebug.cli_color', 2 );
+
 		ob_start();
 		var_dump( $var );
 		$out1 = ob_get_contents();
 		ob_end_clean();
 		error_log( $out1 );
+
+		ini_set( 'xdebug.cli_color', 1 );
+		ini_set( 'html_errors', $old_setting );
 	}
 }
-
-
