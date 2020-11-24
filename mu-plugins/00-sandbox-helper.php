2fffe745da8449b1db0fed2e158f4af6bf1fb1f4
Derrick Tennant <emrikol@users.noreply.github.com>
2020-11-24 15:42:44 -0500

    Update 00-sandbox-helper.php

--- mu-plugins/00-sandbox-helper.php
+++ mu-plugins/00-sandbox-helper.php
@@ -29,3 +29,10 @@ if ( ! function_exists( 'vip_dump' ) ) {
 		ini_set( 'html_errors', $old_setting );
 	}
 }
+
+// Add Sandbox WP Debugger support for internal REST API requests.
+add_action( 'rest_pre_dispatch', function( $result, $server, $request ) {
+	if ( function_exists( 'swpd_log' ) ) {
+		swpd_log( 'rest_do_request', $request->get_route(), $request->get_params(), [], true );
+	}
+}, 10, 3 );
